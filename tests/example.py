e694adb93b1a21772ae777df9349a6a46666c720
Richard Bann <richard.bann@vertis.com>
2019-09-05 20:01:09 +0200

    blackify

--- tests/example.py
+++ tests/example.py
@@ -4,33 +4,42 @@ from rel2tree import F, groupkey
 
 
 data = [
-  {"name": "Jane", "city": "New York", "sales": 23},
-  {"name": "Joe", "city": "New York", "sales": 11},
-  {"name": "Jane", "city": "Chicago", "sales": 21},
-  {"name": "Jane", "city": "New York", "sales": 4},
-  {"name": "Joe", "city": "New York", "sales": 13},
-  {"name": "Joe", "city": "Chicago", "sales": 31},
-  {"name": "Jane", "city": "New York", "sales": 7},
+    {"name": "Jane", "city": "New York", "sales": 23},
+    {"name": "Joe", "city": "New York", "sales": 11},
+    {"name": "Jane", "city": "Chicago", "sales": 21},
+    {"name": "Jane", "city": "New York", "sales": 4},
+    {"name": "Joe", "city": "New York", "sales": 13},
+    {"name": "Joe", "city": "Chicago", "sales": 31},
+    {"name": "Jane", "city": "New York", "sales": 7},
 ]
 
-f = F().groupby(lambda x: x["name"], F().dict({
-    "name": groupkey(),
-    "cities": F().groupby(lambda x: x["city"], F().dict({
-        "city": groupkey(),
-        "sales": F().map(lambda x: x["sales"]).t(sum)
-    })),
-    "sum": F().map(lambda x: x["sales"]).t(sum)
-}))
+f = F().groupby(
+    lambda x: x["name"],
+    F().dict(
+        {
+            "name": groupkey(),
+            "cities": F().groupby(
+                lambda x: x["city"],
+                F().dict(
+                    {"city": groupkey(), "sales": F().map(lambda x: x["sales"]).t(sum)}
+                ),
+            ),
+            "sum": F().map(lambda x: x["sales"]).t(sum),
+        }
+    ),
+)
 
 print(json.dumps(f(data)))
 
 
 data = [1, 2, 3, 4]
-f = F().dict({
-    "list": F(),
-    "even": F().filter(lambda x: x % 2 == 0),
-    "odd": F().filter(lambda x: x % 2 != 0),
-})
+f = F().dict(
+    {
+        "list": F(),
+        "even": F().filter(lambda x: x % 2 == 0),
+        "odd": F().filter(lambda x: x % 2 != 0),
+    }
+)
 print(json.dumps(f(data)))
 
 
