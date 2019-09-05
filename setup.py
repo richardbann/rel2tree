38ef09aa0c3dad36659994ff744f349f8316cce0
Richard Bann <richard.bann@vertis.com>
2019-09-05 20:03:27 +0200

    version: 6.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = "\n" + f.read()
 
-VERSION = "6.0.0"
+VERSION = "6.0.1"
 
 setup(
     name="rel2tree",
